94fe6930ab7d064118c7a87bde1ff20346b90ee0
vrcvieira <victor.vieira@ifac.edu.br>
2022-07-06 09:03:38 -0500

    Alterações da aula de 06 de julho.

---
+++ lista-php/lista-01.php
@@ -0,0 +1,9 @@
+<?php
+   
+   $num = $_POST['num'];
+
+   echo $num-1;
+
+   echo $num+1;
+
+?>
---
+++ lista-php/lista-02.php
@@ -0,0 +1,9 @@
+<?php
+
+    $lado = $_POST['lado'];
+
+    echo $lado * 4;
+
+    echo $lado * 2;
+
+?>
