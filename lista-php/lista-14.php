6ac8af19a0a8b27ba2645861b43b88fa2c16a2cf
vrcvieira <victor.vieira@ifac.edu.br>
2022-07-20 10:42:40 -0500

    Códigos de 20 de julho.

--- lista-php/lista-14.php
+++ lista-php/lista-14.php
@@ -3,10 +3,8 @@
     // Temos que pegar todo mundo que está vindo do formulário.
 
     for($i=1;$i<=7;$i++) {
-        $n[$i] = $_POST['n\$\i']; // O código precisa de ajustes aqui.
-        echo $n[$i];
+        $n[$i] = $_POST['n{$i}']; // O código precisa de ajustes aqui.
+        echo $n[$i]^2;
     }
-
-    // Está incompleto. Ainda não resolve a questão.
 
 ?>
